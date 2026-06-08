2c41d5ffb5f23a64962a28bfdb5c254df7832fc1
jainarihantt <arihant@wokay.com>
2026-06-08 13:54:23 +0530

    Feat add google tag manager

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,11 +5,14 @@ import {
   Noto_Sans_Devanagari,
   Fraunces,
 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Providers from "@/lib/providers";
 import { PromoBannerBar } from "@/components/ui/promo-banner-bar";
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+
+const GTM_ID = "GTM-NW4D5W7J";
 const hostGrotesk = Host_Grotesk({
   variable: "--font-heading",
   subsets: ["latin"],
@@ -112,7 +115,26 @@ export default function RootLayout({
       lang="en"
       className={`${hostGrotesk.variable} ${dmSans.variable} ${notoDevanagari.variable} ${fraunces.variable} h-full scroll-smooth antialiased`}
     >
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+        }}
+      />
       <body className="min-h-full flex flex-col bg-transparent">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers>
           <Analytics />
           <SpeedInsights />
